e2112d955f6c6f363ece809e3b82fda05bce04c7
JackBrasesco <jacbras@nuevaschool.org>
2016-12-07 08:47:39 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ $("#text").keydown(function() {
   var whatToDraw = $("#text").val()
   var isSquare = whatToDraw.indexOf("square");
   if (isSquare > 0) {
-    goForw
+    goForward(300)
+    turnLeft()
+    goForward(100)
+    turnLeft()
+    goForward(100)
+    turnLeft()
+    goForward(100)
   }
 })
